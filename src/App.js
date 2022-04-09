231a9f563de7a358b476de9a43ba58f58b5163f9
Stiven Ruiz <stiven45ruiz@gmail.com>
2022-04-08 21:10:33 -0500

    Componentes de TODO Machine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from './logo.svg';
+//import './App.css';
+
+const todos = [
+  { text: 'Cortar cebolla', complete: false },
+  { text: 'Tomar Curso de React', complete: false },
+  { text: 'Llorar con la llorona', complete: false }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {/*<TodoCounter/>*/}
+      <h2>Has completado 2 de 3 TODOs</h2>
+      {/* <TodoSearch/> */}
+      <input placeholder="Cebolla"/>
+      {/* <TodoList>
+        {todos.map(todo => (
+          <todoItem/>
+        ))}
+      </TodoList> */}
+      {/* <CreatetodoButton/> */}
+      <button>+</button>
+    </React.Fragment>
   );
 }
 
