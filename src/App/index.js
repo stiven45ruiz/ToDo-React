7f5871f4e8928370bf0f44b2e7085b8345b947dc
Stiven Ruiz <stiven45ruiz@gmail.com>
2022-04-19 15:23:08 -0500

    React Context: estado compartido

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import {TodoProvider} from '../TodoContext'
 import { AppUI } from './AppUI';
+
 
 // const defaultTodos = [
 //   { text: 'Cortar cebolla', completed: true },
@@ -8,77 +10,17 @@ import { AppUI } from './AppUI';
 //   { text: 'LALALALAA', completed: false },
 // ];
 
-function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
-  
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
 
-  const [item, setItem] = React.useState(parsedItem);
-
-  const saveItem = (newItem) => {
-    const stringifiedItem = JSON.stringify(newItem);
-    localStorage.setItem(itemName, stringifiedItem);
-    setItem(newItem);
-  };
-
-  return [
-    item,
-    saveItem,
-  ];
-}
 
 function App() {
-  const [patito, savePatito] = useLocalStorage('PATITO_V1', 'FERNANDO');
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
-  const [searchValue, setSearchValue] = React.useState('');
-
-  const completedTodos = todos.filter(todo => !!todo.completed).length;
-  const totalTodos = todos.length;
-
-  let searchedTodos = [];
-
-  if (!searchValue.length >= 1) {
-    searchedTodos = todos;
-  } else {
-    searchedTodos = todos.filter(todo => {
-      const todoText = todo.text.toLowerCase();
-      const searchText = searchValue.toLowerCase();
-      return todoText.includes(searchText);
-    });
-  }
-
-  const completeTodo = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
-    const newTodos = [...todos];
-    newTodos[todoIndex].completed = true;
-    saveTodos(newTodos);
-  };
-
-  const deleteTodo = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
-    const newTodos = [...todos];
-    newTodos.splice(todoIndex, 1);
-    saveTodos(newTodos);
-  };
   
-  return [
-    <p>{patito}</p>,
-    <AppUI
-      totalTodos={totalTodos}
-      completedTodos={completedTodos}
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-      searchedTodos={searchedTodos}
-      completeTodo={completeTodo}
-      deleteTodo={deleteTodo}
-    />,
-  ];
+  
+  
+  return (
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
+  );
 }
 
 export default App;
