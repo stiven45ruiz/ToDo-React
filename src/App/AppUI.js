10ee286bcf4729160bb72fe8ce1c7d3cd338a2e2
Stiven Ruiz <stiven45ruiz@gmail.com>
2022-04-19 11:12:15 -0500

    CustomHook para localStorage

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,43 +1,45 @@
-import React from "react";
-import { TodoCounter } from "../TodoCounter";
-import { TodoSearch } from "../TodoSearch";
-import { TodoItem } from "../TodoItem";
-import { TodoList } from "../TodoList";
-import { CreatetodoButton } from "../CreatetodoButton";
+import React from 'react';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { CreateTodoButton } from '../CreateTodoButton';
 
 function AppUI({
-        totalTodos,
-        completedTodos,
-        searchValue,
-        setSearchValue,
-        searchedTodo,
-        completeTodo,
-        deleteTodo,
-    }){   
-    return(
-      <React.Fragment>
-        <TodoCounter
-          total={totalTodos}//cada ves que cambie cualquier estado, nos envia a totalTodos
-          completed={completedTodos}//Nos envia las info de todos completados
-        />
-        <TodoSearch
-          searchValue={searchValue}//Es igaula al valor
-          setSearchValue={setSearchValue}// es igual a la función
-        />
-        <TodoList>
-          {searchedTodo.map(todo => (
-            <TodoItem 
-              key={todo.text} 
-              text={todo.text}
-              completed={todo.completed}
-              onComplete={()=> completeTodo(todo.text)}
-              onDelete={()=> deleteTodo(todo.text)}
-            />
-          ))}
-        </TodoList>
-        <CreatetodoButton/>
-      </React.Fragment>   
-    );
-};
+  totalTodos,
+  completedTodos,
+  searchValue,
+  setSearchValue,
+  searchedTodos,
+  completeTodo,
+  deleteTodo,
+}) {
+  return (
+    <React.Fragment>
+      <TodoCounter
+        total={totalTodos}
+        completed={completedTodos}
+      />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
-export {AppUI};
+      <TodoList>
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
+  );
+}
+
+export { AppUI };
