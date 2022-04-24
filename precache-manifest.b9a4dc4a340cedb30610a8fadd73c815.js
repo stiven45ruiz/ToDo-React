60b24b5e389cd150537aac8449a550d15f7324c1
stiven45ruiz <stiven45ruiz@gmail.com>
2022-04-24 15:36:58 -0500

    Updates

---
+++ precache-manifest.b9a4dc4a340cedb30610a8fadd73c815.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "daf466ef93d64fcae09f",
+    "url": "/stiven45ruiz/ToDo-React/static/css/main.947e2aa2.chunk.css"
+  },
+  {
+    "revision": "daf466ef93d64fcae09f",
+    "url": "/stiven45ruiz/ToDo-React/static/js/main.daf466ef.chunk.js"
+  },
+  {
+    "revision": "1d9d3bc05754e93ba53c",
+    "url": "/stiven45ruiz/ToDo-React/static/js/1.1d9d3bc0.chunk.js"
+  },
+  {
+    "revision": "2ed4de240af3d9ab2ae7",
+    "url": "/stiven45ruiz/ToDo-React/static/js/runtime~main.2ed4de24.js"
+  },
+  {
+    "revision": "75e00ab5d22f30f637bc87ae395dd84b",
+    "url": "/stiven45ruiz/ToDo-React/index.html"
+  }
+];
